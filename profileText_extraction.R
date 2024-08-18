8aecc5e9fbc469897c13392d6856b5c8e8d637fb
hedeenza <hedeenza@gmail.com>
2024-08-18 18:01:13 -0400

    starting the widening of the table columns.

--- profileText_extraction.R
+++ profileText_extraction.R
@@ -13,11 +13,43 @@ sections <-
     too_few = "align_start",
     too_many = "drop")
 
-# Separating out the position 
+# Creating a proof of concept tester
+tester <- sections |> head(n=20)
 
-  sections |>
-  str_view("Senator|Representative") |>
-    head()
+# Detecting the position 
+tester$profileText_1 |> str_detect("Representative|Senator")
+
+# Extracting just the position name
+tester$profileText_1 |> str_extract("Representative|Senator")
 
 
-table(position$profileText_1_38)
+# Experimenting with what to separate, and in what order 
+year_split <-
+  tester$profileText_2 |> 
+  str_remove_all("\\bborn\\b|\\bin\\b|\\bnear\\b") |> # includes the boundaries around the words, so "in" doesn't get removed from the middle of a word
+  str_replace_all(" ([0123456789][0123456789][0123456789][0123456789])", "*\\1") |> # four of any digit in a row is the year, detecting a space followed by the year
+  str_trim() |>
+  as.data.frame() |>
+  separate_wider_delim(
+    cols = 1, 
+    names_sep = "_",
+    delim = "*",
+    too_few = "align_start",
+    too_many = "drop"
+  ) # separating off the year
+
+# 
+year_split
+  
+  
+
+str_replace_all("January|February|March|April|May|June|July|August|September|October|November|December", "-") |>
+  str_replace_all("\\, -", "*") |>
+  str_trim() |>
+  as.data.frame() |> 
+  separate_wider_delim(
+    cols = 1,
+    names_sep = "_",
+    delim = "*",
+    too_few = "align_start",
+    too_many = "drop")
